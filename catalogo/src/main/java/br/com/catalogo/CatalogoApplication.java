2e54e2fe7174ac381ad50d12ffd732c22e766dd4
Eduardo Magalhaes <52082458+edudevhome@users.noreply.github.com>
2022-09-05 16:12:19 -0300

    initial commit.

---
+++ catalogo/src/main/java/br/com/catalogo/CatalogoApplication.java
@@ -0,0 +1,33 @@
+package br.com.catalogo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.catalogo.model.Category;
+import br.com.catalogo.repository.CategoryRepository;
+
+@SpringBootApplication
+public class CatalogoApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category category = new Category(1L, "Eletronics");
+		Category category1 = new Category(2L, "Books");
+		Category category2 = new Category(3L, "Foods");
+		
+		categoryRepository.save(category);
+		categoryRepository.save(category1);
+		categoryRepository.save(category2);
+	}
+
+}
---
+++ catalogo/src/main/java/br/com/catalogo/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package br.com.catalogo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.catalogo.model.Category;
+import br.com.catalogo.repository.CategoryRepository;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+
+		 List<Category> list = categoryRepository.findAll();
+//		list.add(new Category(1L, "Eletronics"));
+//		list.add(new Category(2L, "Books"));
+//		list.add(new Category(3L, "Foods"));
+
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping("/{idCategory}")
+	public ResponseEntity<Category> findById(@PathVariable Long idCategory) {
+
+		Category category = categoryRepository.findById(idCategory);
+		return ResponseEntity.ok().body(category);
+	}
+
+}
